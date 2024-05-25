dc32f33b80dcd23c3055a0231589b18f0639e859
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-05-25 11:53:58 +0800

    added users template

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:username', function(req, res, next) {
+  const userName = req.params.username;
+  res.render('users', {username : userName})
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
