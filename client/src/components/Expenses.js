a58cb248321cca79f9739199b3239b25023f2e6a
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-06-05 13:36:04 +0800

    testing changes

--- client/src/components/Expenses.js
+++ client/src/components/Expenses.js
@@ -2,7 +2,7 @@ export default function ProjectAbout() {
     return <>
         <div class="container">
             <h1>Expenses</h1>
-            <h2>TEsting 1 2 3</h2>
+            <h2>Hello world!</h2>
         </div>
     </>
 }
