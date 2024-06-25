2a050ae73a8c24ffb5585aa2232cd56e4672dd66
BiggillyBoyMan <lucaslow57@gmail.com>
2024-06-25 20:17:09 +0800

    Expenses New Updates V1

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./styles.css"
+import { BudgetsProvider } from "./context/BudgetsContext"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BudgetsProvider>
+      <App />
+    </BudgetsProvider>
   </React.StrictMode>
   // <App />
 );
