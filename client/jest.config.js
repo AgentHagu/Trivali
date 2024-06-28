b3f7f45420341c71cc76ec4f6e9c05c4dd4bd2fd
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-06-28 15:21:58 +0800

    removed tests

--- client/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    roots: ['<rootDir>/tests'],
-    setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
-    testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
-  };
-  
